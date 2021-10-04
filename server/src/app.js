414f4d2839cf9078d30a1d47dd9898efad1a5661
Brett Hoes <bretthoes@gmail.com>
2021-10-03 22:37:21 -0400

    update for prod env, remove dist for gitignore

--- server/src/app.js
+++ server/src/app.js
@@ -2,23 +2,27 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors =  require('cors')
 const morgan = require('morgan')
+const path = require('path')
 const {sequelize} = require('./models')
 const config = require('./config/config')
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
-app.use('/uploads', express.static('uploads'))
 
 const router = express.Router();
-router.get('/', function(req,res,next){
+router.get('/', function(req, res, next){
  req.url = '/index.html';
  next();
-});
-
-app.use(cors())
+})
 
 app.use(router)
-app.use(express.static('dist'))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, "../dist")))
+  console.log(process.env.NODE_ENV)
+}
+
+app.use(cors())
 
 // Pass our app and attach all endpoints
 require('./routes')(app)
@@ -27,5 +31,5 @@ require('./routes')(app)
 sequelize.sync({force: false})
   .then(() => {
     app.listen(config.port)
-    console.log('Server started on port ' + config.PORT)
+    console.log('Server started on port ' + config.port)
   })
