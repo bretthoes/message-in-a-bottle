cef096c5987f7b63f83b30c7be3b7f51c05b841b
Brett Hoes <bretthoes@gmail.com>
2023-08-18 23:30:51 -0400

    update prod base url to render

--- client/config/prod.env.js
+++ client/config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_URL: '"https://capstone-000794169.herokuapp.com"',
+  BASE_URL: '"https://message-in-a-bottle.onrender.com"',
   MAX_UPLOAD_BYTES: '5000000'
 }
